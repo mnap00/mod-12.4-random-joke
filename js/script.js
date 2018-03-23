f22b912985d8390dfd5a33b98c14bca3605af5fe
mnap <learn@10g.pl>
2018-03-23 21:17:10 +0100

    Update project with jQuery

--- js/script.js
+++ js/script.js
@@ -1,20 +1,19 @@
 var url = 'http://api.icndb.com/jokes/random';
 
-var button = document.getElementById('get-joke');
-button.addEventListener('click', function() {
+var $button = $('#get-joke').click(function () {
     getJoke();
 });
 
-var paragraph = document.getElementById('joke');
+var $paragraph = $('#joke');
 
 function getJoke() {
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', url);
-    xhr.addEventListener('load', function() {
-        var response = JSON.parse(xhr.response);
-        paragraph.innerHTML = response.value.joke;
+    $.ajax({
+        method: 'GET',
+        url: url,
+        success: function(res) {  // res -> response
+            $paragraph.text(res.value.joke);
+        }
     });
-    xhr.send();
 }
 
 getJoke();
